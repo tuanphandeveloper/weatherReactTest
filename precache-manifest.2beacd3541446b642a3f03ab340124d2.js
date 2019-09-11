a5807c6cda608ece1531caf369ab1d71673d0f2b
Tuan Phan <tuan.p01@gmail.com>
2019-09-11 00:10:55 -0600

    Updates

---
+++ precache-manifest.2beacd3541446b642a3f03ab340124d2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5193b001e75e75da5056b16b0e7e67db",
+    "url": "/weatherReactTest/index.html"
+  },
+  {
+    "revision": "e44576bc5025b7202f5b",
+    "url": "/weatherReactTest/static/css/2.e4ede677.chunk.css"
+  },
+  {
+    "revision": "8a26ce4489f1efbcdad1",
+    "url": "/weatherReactTest/static/css/main.317b2a24.chunk.css"
+  },
+  {
+    "revision": "e44576bc5025b7202f5b",
+    "url": "/weatherReactTest/static/js/2.16fa4b8a.chunk.js"
+  },
+  {
+    "revision": "8a26ce4489f1efbcdad1",
+    "url": "/weatherReactTest/static/js/main.6f401c5d.chunk.js"
+  },
+  {
+    "revision": "1f1df6cedc8d0c9bb8ae",
+    "url": "/weatherReactTest/static/js/runtime~main.6da4ad3d.js"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/weatherReactTest/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/weatherReactTest/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/weatherReactTest/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/weatherReactTest/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/weatherReactTest/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  }
+]);
